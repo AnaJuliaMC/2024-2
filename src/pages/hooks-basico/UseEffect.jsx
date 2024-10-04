17e6363a93c29f3e0fb7c3b6011c0b5b09d25137
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-10-04 09:56:30 -0300

    Update UseEffect.jsx

--- src/pages/hooks-basico/UseEffect.jsx
+++ src/pages/hooks-basico/UseEffect.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react"
 import { Pagina } from "../../componentes/Pagina"
+
 export function UseEffect(){
+    const [url, setUrl] = useState("")
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resposta => resposta.json())
+        .then(objeto => {
+            console.log(objeto)
+            setUrl(objeto.message)})
+    }, [])
     return(
         <Pagina titulo="useEffect" subtitulo="Hooks Básicos"> 
+        <img src={url} alt="" />
         </Pagina>
     )
 }
